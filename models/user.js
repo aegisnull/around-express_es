36f8d1dd3d135f0e43ac0686b98417266e85456a
Luis Tellez <luis@luistellez.com>
2022-11-04 12:26:02 -0700

    feat: create cardSchema

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /(http|https):\/\/(www\.)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/.test(
+          v
+        );
+      },
+    },
+  },
+});

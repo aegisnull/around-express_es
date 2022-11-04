b168d817dccecc8de26f3694bf995a55cf46925c
Luis Tellez <luis@luistellez.com>
2022-11-04 15:21:10 -0700

    refactor: introduce users controller and models

---
+++ controllers/users.js
@@ -0,0 +1,70 @@
+const User = require("../models/user");
+
+module.exports.getUsers = (req, res) =>
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: `Error: ${err}` }));
+
+module.exports.getUserById = (req, res) =>
+  User.findById(req.params.id)
+    .orFail(() => {
+      const error = new Error("No user found with that id");
+      error.statusCode = 404;
+      throw error;
+    })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        res.status(400);
+      } else {
+        res.status(500);
+      }
+      res.send({ message: `Error: ${err}` });
+    });
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400);
+      } else {
+        res.status(500);
+      }
+      res.send({ message: `Error: ${err}` });
+    });
+};
+
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+
+  const opts = { runValidators: true, new: true };
+  User.findByIdAndUpdate(req.user._id, { name, about }, opts)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400);
+      } else {
+        res.status(500);
+      }
+      res.send({ message: `Error: ${err}` });
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  const opts = { runValidators: true, new: true };
+  User.findByIdAndUpdate(req.user._id, { avatar }, opts)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400);
+      } else {
+        res.status(500);
+      }
+      res.send({ message: `Error: ${err}` });
+    });
+};
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,16 @@
-const usersRouter = require("express").Router();
-const fsPromises = require("fs").promises;
-const { resolvePtr } = require("dns");
-const path = require("path");
+const router = require("express").Router();
+const {
+  getUsers,
+  getUserById,
+  createUser,
+  updateProfile,
+  updateAvatar,
+} = require("../controllers/users");
 
-const usersPath = path.join(__dirname, "../data/users.json");
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
+router.patch("/me", updateProfile);
+router.patch("/me/avatar", updateAvatar);
 
-usersRouter.get("/users", (req, res) => {
-  fsPromises
-    .readFile(usersPath, { encoding: "utf8" })
-    .then((data) => {
-      res.json(JSON.parse(data));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-usersRouter.get("/users/:_id", (req, res) => {
-  const { _id } = req.params;
-  const user = require("../data/users.json").find((user) => user._id === _id);
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: "ID de usuario no encontrado" });
-  }
-});
-
-module.exports = usersRouter;
+module.exports = router;
